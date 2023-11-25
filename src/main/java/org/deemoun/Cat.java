9610062feac079e7015c29bc49933cfc8ecbc09f
Dmitry Y. <deemoun@users.noreply.github.com>
2023-11-25 12:20:55 +0100

    Add comments. Modify Cat class

--- src/main/java/org/deemoun/Cat.java
+++ src/main/java/org/deemoun/Cat.java
@@ -1,15 +1,21 @@
 package org.deemoun;
 
+/* Класс КОТ наследуется от класса Animal
+   Это значит, что все методы из класса Animal - доступны нам
+ */
 public class Cat extends Animal {
+    /* В классе Cat - метод getMeow и setMeow ДОПОЛНЯЮТ функции из класса Animal
+     Таким образом - мы добавляем новый функционал в наш класс
+     */
+    private String meow;
 
-    private String name;
-
-    public String getName() {
-        return "Cat name is: " + name;
+    // Здесь снова используются геттеры и сеттеры, как и в классе Animal
+    public String getMeow() {
+        return "Cat meow is: " + meow;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setMeow(String name) {
+        this.meow = name;
     }
 
 }
