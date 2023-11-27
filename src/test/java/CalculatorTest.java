d6af8a1cdf71b101aa5162b17121b3ab85a0d07c
Dmitry Y. <deemoun@users.noreply.github.com>
2023-11-27 14:16:36 +0100

    Add files to project

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,6 @@
 import org.deemoun.Calculator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
